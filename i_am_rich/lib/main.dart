08e800e2be3ff33ee0eda76c0941c6267ef66caa
Andrew Enrique Oliveira <andrew@192.168.1.7>
2022-09-26 19:30:28 -0300

    Udemy - app 1

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Eu sou rico"),
+          backgroundColor: Colors.purple[700],
+        ),
+        backgroundColor: Colors.purple[100],
+        body: const Center(
+          child: Image(
+            image: NetworkImage('https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'),
+          ),
+        ),
+      ), // Esqueleto
+    ),
+  );
+}
