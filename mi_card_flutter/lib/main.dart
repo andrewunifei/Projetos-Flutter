c44de0fd02cd58c3ce1abf01abd01ecdb4059404
Andrew Enrique Oliveira <andrew@192.168.1.7>
2022-09-27 14:00:22 -0300

    Projeto mi card

---
+++ mi_card_flutter/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100.0,
+                height: double.infinity,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.yellow,
+                    width: 100.0,
+                    height: 100.0,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    width: 100.0,
+                    height: 100.0,
+                  ),
+                ]
+              ),
+              Container(
+                color: Colors.purple,
+                width: 100.0,
+                height: double.infinity,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
