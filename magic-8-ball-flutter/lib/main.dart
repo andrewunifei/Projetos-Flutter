70cb79444f1b6604095a4068b3325c3c5a7496ca
Andrew <andrewunifei@gmail.com>
2022-10-02 13:31:55 -0300

    Projeto magic ball

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Me pergunte qualquer coisa'),
+          backgroundColor: Colors.purple[900],
+      ),
+      body: Ball()
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballState = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.purple[700],
+      child: TextButton(
+        onPressed: (){
+          setState(() {
+            ballState = Random().nextInt(5) + 1;
+          });
+        },
+        child: Center(
+            child: Image.asset('images/ball$ballState.png')
+        ),
+      )
+    );
+  }
+}
+
